4affb694765b2c8a6cb252dbf37b39e3e4a9a1da
VysochenkoDanil <czzzrcoooll@inbox.ru>
2025-05-11 15:14:08 +0300

    исправление env.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,12 +3,11 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-# SECRET_KEY = os.getenv('SECRET_KEY')
+
 DEBUG = False
 
 ALLOWED_HOSTS = [
