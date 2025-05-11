002de13ca3f8296908238b7fed01ad5041b3f562
VysochenkoDanil <czzzrcoooll@inbox.ru>
2025-05-11 17:12:49 +0300

    эксперименьальная версия_5

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,13 +3,12 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-# Сначала определяем BASE_DIR
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+# Определяем BASE_DIR до load_dotenv
+BASE_DIR = Path(__file__).resolve().parent.parent.parent  # Добавляем дополнительный parent
 
-# Затем загружаем .env файл
+# Теперь путь к .env будет корректным
 load_dotenv(dotenv_path=BASE_DIR / '.env')
 
-# Теперь можно использовать переменные окружения
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
